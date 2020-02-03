f1261ec79e1167d5ba86bd74b31ac2aa6b1bd1ba
Sain070-tsk <akpak070@gmail.com>
2020-02-04 01:43:14 +0700

    исправляем ошибки и добавляем функцию вычитания

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-plus(){
+function plus(){
 	//Объявляем переменные
 	var num1, num2, res;
 	//Получаем элемент с индентификатором n1 и присваиваем его переменной num1
@@ -13,18 +13,14 @@ plus(){
 	//Возвращаем результат выполнения на страницу по идентификатору result посредством добавления в конец строки
 	document.getElementById('result').innerHTML = res;	
 }
-minus(){
-	//Объявляем переменные
+
+function minus()
+{
 	var num1, num2, res;
-	//Получаем элемент с индентификатором n1 и присваиваем его переменной num1
 	num1 = document.getElementById('n1').value;
-	//Парсим элемент получаем число из строки введенного текста
 	num1 = parseInt(num1);
-	//Проделываем тоже самое для num2
 	num2 = document.getElementById('n2').value;
 	num2 = parseInt(num2);
-	//Суммируем полученные числа операцие сложения, результат помещаем в переменную res
 	res = num1 - num2;
-	//Возвращаем результат выполнения на страницу по идентификатору result посредством добавления в конец строки
 	document.getElementById('result').innerHTML = res;	
 }
